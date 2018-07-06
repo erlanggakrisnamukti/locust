05004f168fb39adb64ca01478794286b4a254b34
Amadea <amadeakristina@gmail.com>
2018-07-06 18:58:06 +0700

    create test object summary for report's needs

--- locust/report.py
+++ locust/report.py
@@ -31,4 +31,165 @@ class ReportHandler(object):
         for x in self.acceptable_options:
             self._options[x] = getattr(value, x)
 
+class TestSuiteSummary(object):
+    def __init__(self):
+        self.test_suites = []
+        self.total_pass = 0
+        self.total_fail = 0
+        self.total_warning = 0
+
+    @property
+    def test_suites(self):
+        return self.test_suites
+
+    def append_test_suite(self, test_suite):
+        self.test_suites.append(test_suite)
+
+    @property
+    def total_fail(self):
+        return self.total_fail
+    
+    @total_fail.setter
+    def total_fail(self, value):
+        self.total_fail = value
+    
+    @property
+    def total_pass(self):
+        return self.total_pass
+    
+    @total_pass.setter
+    def total_pass(self, value):
+        self.total_pass = value
+
+    @property
+    def total_warning(self):
+        return self.total_warning
+    
+    @total_warning.setter
+    def total_warning(self, value):
+        self.total_warning = value
+    
+
+class TestCaseSummary(object):
+    def __init__(self):
+        self.test_cases = []
+        self.repetition = 0
+        self.total_fail = 0
+        self.total_pass = 0
+        self.status = None
+        self.total_duration = 0
+        self.test_steps = []
+    
+    @property
+    def test_cases(self):
+        return self.test_cases
+
+    def append_test_case(self, test_case):
+        self.test_cases.append(test_case)
+
+    @property
+    def test_steps(self):
+        return self.test_steps
+    
+    def append_test_step(self, test_step):
+        self.test_steps.append(test_step)
+    
+    @property
+    def repetition(self):
+        return self.repetition
+
+    @repetition.setter
+    def repetition(self, value):
+        self.repetition = value
+    
+    @property
+    def total_fail(self):
+        return self.total_fail
+    
+    @total_fail.setter
+    def total_fail(self, value):
+        self.total_fail = value
+    
+    @property
+    def total_pass(self):
+        return self.total_pass
+    
+    @total_pass.setter
+    def total_pass(self, value):
+        self.total_pass = value
+
+    @property
+    def status(self):
+        return self.status
+
+    @status.setter
+    def status(self, value):
+        self.status = value
+
+    @property
+    def total_duration(self):
+        return self.total_duration
+    
+    @total_pass.setter
+    def total_duration(self, value):
+        self.total_duration = value
+
+
+class TestStepSummary(object):
+    def __init__(self):
+        self.test_steps = []
+        self.repetition = 0
+        self.total_fail = 0
+        self.total_pass = 0
+        self.status = None
+        self.total_duration = 0
+
+    @property
+    def test_steps(self):
+        return self.test_steps
+    
+    def append_test_step(self, test_step):
+        self.test_steps.append(test_step)
+    
+    @property
+    def repetition(self):
+        return self.repetition
+
+    @repetition.setter
+    def repetition(self, value):
+        self.repetition = value
+    
+    @property
+    def total_fail(self):
+        return self.total_fail
+    
+    @total_fail.setter
+    def total_fail(self, value):
+        self.total_fail = value
+    
+    @property
+    def total_pass(self):
+        return self.total_pass
+    
+    @total_pass.setter
+    def total_pass(self, value):
+        self.total_pass = value
+
+    @property
+    def status(self):
+        return self.status
+
+    @status.setter
+    def status(self, value):
+        self.status = value
+
+    @property
+    def total_duration(self):
+        return self.total_duration
+    
+    @total_pass.setter
+    def total_duration(self, value):
+        self.total_duration = value
+
+
 report = ReportHandler()
